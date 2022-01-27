fcd564c0fec49d1fa345a9608d68130965529900
flamechain <rwc@thezulus.com>
2022-01-26 22:55:06 -0800

    fixed DrawElipse

--- include/circle.h
+++ include/circle.h
@@ -3,7 +3,6 @@
 
 #include "utils.h"
 
-//draw one quadrant arc, and mirror the other 4 quadrants
 /// Draws an elipse
 /// @param renderer global renderer
 /// @param cx       center x
@@ -25,33 +24,32 @@ void DrawElipse(SDL_Renderer * renderer, int cx, int cy, int rx, int ry) {
     int y1 = y;
 
     // repeat until theta >= 90
-    float step = pih/(float)prec; // amount to add to theta each time (degrees)
-    for(theta=step;  theta <= pih;  theta+=step)//step through only a 90 arc (1 quadrant)
-    {
-        //get new point location
-        x1 = (float)radiusX * cosf(theta) + 0.5; //new point (+.5 is a quick rounding method)
-        y1 = (float)radiusY * sinf(theta) + 0.5; //new point (+.5 is a quick rounding method)
+    float step = pih/(float)prec; // degrees
 
-        //draw line from previous point to new point, ONLY if point incremented
-        if( (x != x1) || (y != y1) )//only draw if coordinate changed
-        {
-            SDL_RenderDrawLine(r, x0 + x, y0 - y,    x0 + x1, y0 - y1 );//quadrant TR
-            SDL_RenderDrawLine(r, x0 - x, y0 - y,    x0 - x1, y0 - y1 );//quadrant TL
-            SDL_RenderDrawLine(r, x0 - x, y0 + y,    x0 - x1, y0 + y1 );//quadrant BL
-            SDL_RenderDrawLine(r, x0 + x, y0 + y,    x0 + x1, y0 + y1 );//quadrant BR
+    for (theta=step; theta<=pih; theta+=step) {
+        // new point (+.5 is a quick rounding method)
+        x1 = (float)rx * cosf(theta) + 0.5;
+        y1 = (float)ry * sinf(theta) + 0.5;
+
+        // draw line from previous point to new point, ONLY if point incremented
+        if ( (x != x1) || (y != y1) ) {
+            SDL_RenderDrawLine(renderer, cx + x, cy - y, cx + x1, cy - y1 ); // quadrant TR
+            SDL_RenderDrawLine(renderer, cx - x, cy - y, cx - x1, cy - y1 ); // quadrant TL
+            SDL_RenderDrawLine(renderer, cx - x, cy + y, cx - x1, cy + y1 ); // quadrant BL
+            SDL_RenderDrawLine(renderer, cx + x, cy + y, cx + x1, cy + y1 ); // quadrant BR
         }
-        //save previous points
-        x = x1;//save new previous point
-        y = y1;//save new previous point
+
+        x = x1;
+        y = y1;
     }
-    //arc did not finish because of rounding, so finish the arc
-    if(x!=0)
-    {
-        x=0;
-        SDL_RenderDrawLine(r, x0 + x, y0 - y,    x0 + x1, y0 - y1 );//quadrant TR
-        SDL_RenderDrawLine(r, x0 - x, y0 - y,    x0 - x1, y0 - y1 );//quadrant TL
-        SDL_RenderDrawLine(r, x0 - x, y0 + y,    x0 - x1, y0 + y1 );//quadrant BL
-        SDL_RenderDrawLine(r, x0 + x, y0 + y,    x0 + x1, y0 + y1 );//quadrant BR
+
+    // account for rounding mistakes
+    if (x != 0) {
+        x = 0;
+        SDL_RenderDrawLine(renderer, cx + x, cy - y, cx + x1, cy - y1 ); // quadrant TR
+        SDL_RenderDrawLine(renderer, cx - x, cy - y, cx - x1, cy - y1 ); // quadrant TL1
+        SDL_RenderDrawLine(renderer, cx - x, cy + y, cx - x1, cy + y1 ); // quadrant BL
+        SDL_RenderDrawLine(renderer, cx + x, cy + y, cx + x1, cy + y1 ); // quadrant BR
     }
 }
 
@@ -59,10 +57,31 @@ class Circle {
 private:
 
     SDL_Renderer * renderer_;
+    SDL_Point center_;
+    int r_;
+    Color color_;
+    Color color_backup_;
 
 public:
 
     Circle() {}
+
+    Circle(SDL_Renderer * renderer, int x, int y, int r, Color rgb) {
+        this->renderer_ = renderer;
+        this->color_ = rgb;
+        this->color_backup_ = rgb;
+        this->center_.x = x;
+        this->center_.y = y;
+        this->r_ = r;
+    }
+
+    /// Renders circle outline
+    /// @param x    relative x
+    /// @param y    relative y
+    void Render(int x = 0, int y = 0) {
+        SDL_SetRenderDrawColor(this->renderer_, this->color_.r, this->color_.g, this->color_.b, 255);
+        DrawElipse(this->renderer_, this->center_.x, this->center_.y, this->r_, this->r_);
+    }
 
 };
 
@@ -72,6 +91,7 @@ private:
 public:
 
     CircleGroup() {}
+
 };
 
 #endif
